0a8f43e5521f27ee686f1c90c1a43f0dc580f1fd
SeverinoArmando <severinoarmando3@gmail.com>
2021-08-21 19:08:25 -0300

    Novos arquivos adicionados

---
+++ Exercicio01/type_Inference/inference.js
@@ -0,0 +1,7 @@
+// O type Inference tipa automaticamente as variaveis de acordo
+// com o valor inserido
+function test(numero, frase) {
+    numero = 1;
+    frase = 'faraway';
+}
+test();
---
+++ Exercicio01/type_Inference/inference.ts
@@ -0,0 +1,6 @@
+// O type Inference tipa automaticamente as variaveis de acordo
+// com o valor inserido
+
+function test(numero: number, frase: string){
+}
+
